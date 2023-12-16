e4fdff3b527c59ca94c8a91c04ca451c9ab04522
Sreekanth S <sreekanthsanthosh17@gmail.com>
2023-12-16 13:39:15 +0530

    code update

--- intermediate_code.c
+++ intermediate_code.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 #include <string.h>
 int i = 1, j = 0, no = 0, tmpch = 90;
 char str[100], left[15], right[15];
-void findopr();
-void explore();
-void fleft(int);
-void fright(int);
+
 struct exp
 {
     int pos;
     char op;
 } k[15];
-void main()
+
+void findopr();
+void explore();
+void fleft(int);
+void fright(int);
+
+int main()
 {
-    clrscr();
     printf("\t\tINTERMEDIATE CODE GENERATION\n\n");
     printf("Enter the Expression :");
     scanf("%s", str);
-    printf("The intermediate code:\t\tExpression\n");
+
+    printf("The intermediate code:\n");
     findopr();
     explore();
-    getch();
+
+    return 0;
 }
+
 
 void findopr()
 {
@@ -32,31 +37,42 @@ void findopr()
             k[j].pos = i;
             k[j++].op = ':';
         }
+        else if (str[i] == '=')
+        {
+            k[j].pos = i;
+            k[j++].op = '=';
+        }
+
     for (i = 0; str[i] != '\0'; i++)
         if (str[i] == '/')
         {
             k[j].pos = i;
             k[j++].op = '/';
         }
+
     for (i = 0; str[i] != '\0'; i++)
         if (str[i] == '*')
         {
             k[j].pos = i;
             k[j++].op = '*';
         }
+
     for (i = 0; str[i] != '\0'; i++)
         if (str[i] == '+')
         {
             k[j].pos = i;
             k[j++].op = '+';
         }
+
     for (i = 0; str[i] != '\0'; i++)
         if (str[i] == '-')
         {
             k[j].pos = i;
             k[j++].op = '-';
-        }
+        } 
 }
+
+
 void explore()
 {
     i = 1;
@@ -65,29 +81,31 @@ void explore()
         fleft(k[i].pos);
         fright(k[i].pos);
         str[k[i].pos] = tmpch--;
+
         printf("\t%c := %s%c%s\t\t", str[k[i].pos], left, k[i].op, right);
-        for (j = 0; j < strlen(str); j++)
-            if (str[j] != '$')
-                printf("%c", str[j]);
         printf("\n");
         i++;
     }
     fright(-1);
+
     if (no == 0)
     {
         fleft(strlen(str));
         printf("\t%s := %s", right, left);
-        getch();
+        printf("\n");
         exit(0);
     }
     printf("\t%s := %c", right, str[k[--i].pos]);
-    getch();
+    printf("\n");
 }
+
+
 void fleft(int x)
 {
     int w = 0, flag = 0;
     x--;
-    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '=' && str[x] != '\0' && str[x] != '-' && str[x] != '/' && str[x] != ':')
+
+    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '=' && str[x] != '\0' && str[x] != '-' && str[x] != '/')
     {
         if (str[x] != '$' && flag == 0)
         {
@@ -100,11 +118,12 @@ void fleft(int x)
     }
 }
 
+
 void fright(int x)
 {
     int w = 0, flag = 0;
     x++;
-    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '\0' && str[x] != '=' && str[x] != ':' && str[x] != '-' && str[x] != '/ ')
+    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '\0' && str[x] != '=' && str[x] != '-' && str[x] != '/')
     {
         if (str[x] != '$' && flag == 0)
         {
